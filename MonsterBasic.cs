60890bbcd311c6a50031966c3e2ecd98fdbf16d0
Erik Schaub <ek9000art@gmail.com>
2017-05-09 14:23:21 -0300

    Scripts

---
+++ MonsterBasic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBasic : MonoBehaviour {
+	private Transform player;
+
+	private float life=20f;
+	private GameObject pj;
+	Animator anim;
+	Animator selfanim;
+	// Use this for initialization
+	void Start () {
+		
+		pj = GameObject.FindGameObjectWithTag ("Player");
+		player =pj.transform;
+		anim = pj.GetComponent<Animator>();
+		selfanim = GetComponent<Animator>();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (life <= 0f) {
+			Destroy (this.gameObject);
+
+
+		}
+	}
+
+	void OnTriggerStay(Collider obj) {
+		if(obj.gameObject.tag == "Hitcollider"){
+			
+		transform.LookAt (player);
+		if (anim.GetBool ("IsPunch")) {
+			selfanim.SetBool ("Hit",true);
+				life -= 0.7f;
+
+		}
+		if (selfanim.GetCurrentAnimatorStateInfo (0).IsName ("Hiteado")) {
+			selfanim.SetBool ("Hit",false);
+
+			
+			}}
+	}
+
+}
